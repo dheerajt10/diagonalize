e6ffc1107fc18b0042ce2839cc2d85946f6a4ba8
udbhav-s <udbhavs03@gmail.com>
2025-09-14 05:09:09 -0400

    methods to create passkey

--- frontend/components/createPasskey.ts
+++ frontend/components/createPasskey.ts
@@ -1,26 +1,40 @@
-export const createPasskey = () => {
-  // Availability of `window.PublicKeyCredential` means WebAuthn is usable.
-  // `isUserVerifyingPlatformAuthenticatorAvailable` means the feature detection is usable.
-  // `isConditionalMediationAvailable` means the feature detection is usable.
-  let passkeyAvailable = false;
-  if (
-    window.PublicKeyCredential &&
-    PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable &&
-    PublicKeyCredential.isConditionalMediationAvailable
-  ) {
-    // Check if user verifying platform authenticator is available.
-    Promise.all([
-      PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable(),
-      PublicKeyCredential.isConditionalMediationAvailable(),
-    ]).then((results) => {
-      if (results.every((r) => r === true)) {
-        // Display "Create a new passkey" button
-        passkeyAvailable = true;
-      }
+const sampleCreationOptions = {
+  challenge: "dGVzdGlk",
+  rp: {
+    name: "Example",
+    id: "localhost",
+  },
+  user: {
+    id: "dGVzdGlk",
+    name: "john78",
+    displayName: "John",
+  },
+  pubKeyCredParams: [
+    {
+      alg: -7,
+      type: "public-key",
+    },
+    {
+      alg: -257,
+      type: "public-key",
+    },
+  ],
+  excludeCredentials: [],
+  authenticatorSelection: {
+    authenticatorAttachment: "platform",
+    requireResidentKey: true,
+  },
+};
+
+
+export const createPasskey = async (decoded_options=sampleCreationOptions) => {
+    const options =
+      PublicKeyCredential.parseCreationOptionsFromJSON(decoded_options as any);
+
+    const credential = await navigator.credentials.create({
+      publicKey: options,
     });
-  }
 
-  if (!passkeyAvailable) throw new Error("Your browser does not support passkeys!");
-
-  
+    console.log("Created credential: ");
+    console.log(credential);
 }
